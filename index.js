a6db63e5158bcc5c120acd8e08413a75e00f4990
EA - Arthur Lima Santos <lima_arthur01@educadventista.org>
2023-10-30 15:11:44 -0300

    finalizando o node + express

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const { application } = require("express")
 const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname, "templates")
+
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com o express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
